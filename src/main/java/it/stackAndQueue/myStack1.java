3bfaeef973f42d1165761922b4bd466b93dc8097
MarkJackson-github <markjacksonmailbox@163.com>
2021-10-05 13:22:45 +0800

    update

---
+++ src/main/java/it/stackAndQueue/myStack1.java
@@ -0,0 +1,8 @@
+package it.stackAndQueue;
+
+/**
+ * @author : mark
+ * @Date : 2021/10/3 下午4:05
+ */
+public class myStack1 {
+}
---
+++ src/main/java/it/stackAndQueue/myStack2.java
@@ -0,0 +1,8 @@
+package it.stackAndQueue;
+
+/**
+ * @author : mark
+ * @Date : 2021/10/3 下午5:18
+ */
+public class myStack2 {
+}
