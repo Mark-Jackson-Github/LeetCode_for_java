b16ac7ccb5d94981bf20fac4fe157b289b78e429
mark <mark@Marks-MacBook-Pro.local>
2022-05-08 15:27:19 +0800

    update

--- src/main/java/com/mark/pattern/singleton/LazySingleton.java
+++ src/main/java/com/mark/pattern/singleton/LazySingleton.java
@@ -7,17 +7,34 @@ package com.mark.pattern.singleton;
  */
 public class LazySingleton {
 
-    private LazySingleton(){}
+    private LazySingleton(){
+        System.out.printf(Thread.currentThread().getName());
+    }
 
-    private static LazySingleton lazySingleton;
+    private volatile static LazySingleton lazySingleton;
 
+    //双重检测懒汉锁的模式
     public static LazySingleton getInstance(){
 
         //懒汉模式就是当对象为空的时候才去创建
         if (lazySingleton == null){
-            lazySingleton = new LazySingleton();
+            synchronized (LazySingleton.class){
+                if (lazySingleton == null){
+                    lazySingleton = new LazySingleton();
+                }
+            }
         }
-
         return lazySingleton;
+    }
+
+
+
+    //在多线程情况下
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                lazySingleton.getClass();
+            }).start();
+        }
     }
 }
