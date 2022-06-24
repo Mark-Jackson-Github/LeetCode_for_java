47a59a716650c84379c53057675295dd62b9722f
Mark-Jackson-Github <610150167@qq.com>
2022-06-24 19:04:23 +0800

    update

---
+++ src/main/java/com/mark/DesignPatterns/Principles/Client.java
@@ -0,0 +1,50 @@
+package com.mark.DesignPatterns.Principles;
+
+/**
+ * @author : mark
+ * @Date : 2022/5/12 上午10:20
+ * 设计模式 - 简单工厂模式
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+    //抽象产品
+    public interface Product {
+        void show();
+    }
+
+    //具体产品：ProductA
+    static class ConcreteProduct1 implements Product {
+        public void show() {
+            System.out.println("具体产品1显示...");
+        }
+    }
+
+    //具体产品：ProductB
+    static class ConcreteProduct2 implements Product {
+        public void show() {
+            System.out.println("具体产品2显示...");
+        }
+    }
+
+    final class Const {
+        static final int PRODUCT_A = 0;
+        static final int PRODUCT_B = 1;
+        static final int PRODUCT_C = 2;
+    }
+
+    static class SimpleFactory {
+        public static Product makeProduct(int kind) {
+            switch (kind) {
+                case Const.PRODUCT_A:
+                    return new ConcreteProduct1();
+                case Const.PRODUCT_B:
+                    return new ConcreteProduct2();
+            }
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/mark/DesignPatterns/Principles/SingleResponsibility1.java
@@ -0,0 +1,15 @@
+package com.mark.DesignPatterns.Principles;
+
+/**
+ * @author : mark
+ * @Date : 2022/5/8 下午6:39
+ *
+ * 设计模式 - 七大设计原则 - 单一原则
+ */
+public class SingleResponsibility1 {
+
+    public static void main(String[] args) {
+        //单一职责原则
+        
+    }
+}
